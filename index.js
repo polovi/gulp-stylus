c60932d37c68f42f482818df9c6b04c0c7cbf247
giant-polovincak <tomas.polovincak@giant.cz>
2014-02-06 10:53:01 +0100

    Added optional params for plugin
    silent: show error only in console if true
    compress: compress output file if true

--- index.js
+++ index.js
@@ -2,16 +2,23 @@
 
 var stylus          = require('stylus'),
     gutil           = require('gulp-util'),
-    transform       = require('stream').Transform,
-    bufferstreams   = require('bufferstreams'),
+    PluginError     = gutil.PluginError,
+    log             = gutil.log,
+    Transform       = require('stream').Transform,
+    defaults        = require('lodash.defaults'),
 
     PLUGIN_NAME     = 'gulp-stylus';
 
 
 function gulpstylus(options) {
-  var stream = new transform({ objectMode: true });
+  var stream = new Transform({ objectMode: true }),
+      opts = defaults(options, {
+        compress: false,
+        silent: false
+      });
 
   stream._transform = function(file, unused, done) {
+
     // Pass through if null
     if (file.isNull()) {
       stream.push(file);
@@ -19,23 +26,29 @@ function gulpstylus(options) {
       return;
     }
 
-    if (!file.isStream()) {
-      try {
-        stylus(file.contents.toString('utf8'))
-          .set('filename', file.path)
-          .set('compress', false)
-          .render(function(error, css) {
-            if (error) throw error;
+    if (file.isStream()) {
+      return done(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+    }
 
-            file.path = gutil.replaceExtension(file.path, '.css');
-            file.contents = new Buffer(css);
-            stream.push(file);
-            done();
-          });
-      } catch (error) {
-        gutil.log(String(new gutil.PluginError(PLUGIN_NAME, error)));
+    var s = stylus(file.contents.toString('utf8'))
+              .set('filename', file.path)
+              .set('compress', opts.compress);
+
+    try {
+      s.render(function(err, css) {
+        if (err) throw err;
+
+        file.path = gutil.replaceExtension(file.path, '.css');
+        file.contents = new Buffer(css);
+        stream.push(file);
         done();
+      });
+    } catch(err) {
+      if (opts.silent) {
+        log(String(new PluginError(PLUGIN_NAME, err)));
+        return done();
       }
+      done(new PluginError(PLUGIN_NAME, err));
     }
 
   };
